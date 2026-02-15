cbbda41a6b4891fd4a7b73a0a06854b7dd8e019a
leonidkavalera-ops <leonidkavalera@gmail.com>
2026-02-15 02:54:22 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'ТВОЙ_SUPABASE_URL';
+const supabaseUrl = 'ТВОЙ_URL';
 const supabaseAnonKey = 'ТВОЙ_ANON_KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
